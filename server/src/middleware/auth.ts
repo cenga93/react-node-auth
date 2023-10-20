2eda123b9dd5bb2cbeff7e82cbaaa56d2027724b
Milan Milic <cenga93@gmail.com>
2023-10-20 11:20:59 +0200

    Removed console.log

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,7 +6,5 @@ import { IReq, IUser } from '../types';
 export default () => (req: IReq | any, res: Response, next: NextFunction) => {
    req.user = <IUser>jwt.verify(req.cookies.token, config.JWT_SECRET_KEY);
 
-   console.log(req.user);
-
    next();
 };
