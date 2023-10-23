e9ef8d57d1f9275e2500c9d28195901c5a20f815
Dale Mitchell <dale.mitchell130@gmail.com>
2023-10-23 12:35:09 +0200

    Updated req header.

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
    res.header('Access-Control-Allow-Origin', origin);
-   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   res.header('Access-Control-Allow-Methods', 'GET, POST');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
    res.header('Access-Control-Allow-Credentials', 'true');
 
